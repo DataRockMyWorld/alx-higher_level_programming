7d84a271b2fd32b43b5a8f5ef3bedfe1de44a2e1
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-02 23:03:45 +0000

    solve task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @list: list
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+        listint_t *temp = NULL;
+
+        if (list == NULL)
+        {
+                return (NULL);
+        }
+
+        temp = list;
+
+        while (temp->next != NULL)
+        {
+                if (temp->next == list)
+                {
+                        return (1);
+                }
+                else
+                {
+                        temp = temp->next;
+                }
+
+        }
+        return (0);
+}
