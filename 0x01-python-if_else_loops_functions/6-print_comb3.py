5e5eaf14b96c54800ed44f011fb8bf019ffb8863
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-03 14:42:57 +0000

    fix new line

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,10 +3,9 @@ i = 0
 while i < 9:
     j = 0
     while j < 10:
-        if (i < j) and (i != j):
-            print("{}{},".format(i, j), end=" ")
         if (i + j) == 17:
             print("{}{}".format(i, j))
+        elif (i < j) and (i != j):
+            print("{}{},".format(i, j), end=" ")
         j = j + 1
     i = i + 1
-
