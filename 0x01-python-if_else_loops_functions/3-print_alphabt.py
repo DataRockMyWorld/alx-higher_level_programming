b10926780c9e4961e968f9aeff87fd693e469d88
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-03 13:15:50 +0000

    fix string format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in "abcdefghijklmnopqrstuvwxyz":
-    if i == 'e' or i == 'q':
+    if i == "e" or i == "q":
         continue
     else:
-        print(f"{i.lower()}", end="")
+        print("{}".format(i), end="")
