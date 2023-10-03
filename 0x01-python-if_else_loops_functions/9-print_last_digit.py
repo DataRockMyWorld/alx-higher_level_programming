a9f8f464b5ebc1a0866446fd02243daacd0264a5
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-03 17:45:31 +0000

    solve task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    try:
+        y = int(number)
+    except ValueError:
+        print("ValueError - wrong input")
+    else:
+        y = abs(y)
+        last = y % 10
+        if y > 0:
+            print(f"{last}", end="")
+            return last
+        elif y < 0:
+            print(f"{last}", end="")
+            return last
+        else:
+            print("0", end="")
+            return 0
