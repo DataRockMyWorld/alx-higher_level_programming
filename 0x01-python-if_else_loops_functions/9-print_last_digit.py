31298c61b4f217c2f6ee6a26ae30a0f4c8d3ce41
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-03 17:51:59 +0000

    remove try & except

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    try:
-        y = int(number)
-    except ValueError:
-        print("ValueError - wrong input")
+    y = int(number)
+    y = abs(y)
+    last = y % 10
+    if y > 0:
+        print(f"{last}", end="")
+        return last
+    elif y < 0:
+        print(f"{last}", end="")
+        return last
     else:
-        y = abs(y)
-        last = y % 10
-        if y > 0:
-            print(f"{last}", end="")
-            return last
-        elif y < 0:
-            print(f"{last}", end="")
-            return last
-        else:
-            print("0", end="")
-            return 0
+        print("0", end="")
+        return 0
