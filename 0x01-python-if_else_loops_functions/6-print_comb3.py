7a4baf54956ff8b213571db962105075aa1aa678
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-03 14:27:42 +0000

    fix string format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 i = 0
-while (i < 10):
+while i < 9:
     j = 0
-    while (j < 10):
+    while j < 10:
         if (i < j) and (i != j):
-            print(f"{i}{j},", end=" ")
-        elif (i + j) == 17:
-            print(f"{i}{j}")
+            print("{}{},".format(i, j), end=" ")
+        if (i + j) == 17:
+            print("{}{}".format(i, j))
         j = j + 1
     i = i + 1
 print()
