c65401f54cafc25ce32b66ee270347c2228405c2
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-07 10:11:22 +0000

    fix length of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,10 @@
 
 
 def element_at(my_list, idx):
-    x = len(my_list)
+    
     if not my_list:
         return
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    if idx > x:
-        return None
+    
     return my_list[idx]
