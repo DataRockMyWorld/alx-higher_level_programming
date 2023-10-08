2cf946f555e0b5860d28d7e529f52b68eaec0b96
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-08 12:44:15 +0000

    change while loop to for

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return
 
     for row in matrix:
+        i = 0
         for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+            if i == 2:
+                print("{:d}".format(element))
+            else:
+                print("{:d}".format(element), end=" ")
+            i += 1
