2fcf44b1813b03a78464fff80e2ad5ff2345f039
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-10 16:52:21 +0000

    solve advanced tax

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ * print_python_list_info - print information on python list
+ * @p: list
+ *
+ * Return: No return.
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *ll;
+	int i;
+
+	ll = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %ld\n", ll->ob_base.ob_size);
+	printf("[*] allocated = %ld\n", ll->allocated);
+	for (i = 0; i < ll->ob_base.ob_size; i++)
+	{
+		printf("Element %d = %s\n", i, ll->ob_item[i]->ob_type->tp_name);
+	}
+}
