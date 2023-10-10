f8bf2a4348a980e6e89268cb02c3a43108ac4981
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-10 19:08:43 +0000

    fix typo error

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "Python.h"
-
 /**
  * print_python_list_info - print information on python list
  * @p: list
@@ -15,9 +14,9 @@ void print_python_list_info(PyObject *p)
 	ll = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %ld\n", ll->ob_base.ob_size);
-	printf("[*] allocated = %ld\n", ll->allocated);
+	printf("[*] Allocated = %ld\n", ll->allocated);
 	for (i = 0; i < ll->ob_base.ob_size; i++)
 	{
-		printf("Element %d = %s\n", i, ll->ob_item[i]->ob_type->tp_name);
+		printf("Element %d: %s\n", i, ll->ob_item[i]->ob_type->tp_name);
 	}
 }
