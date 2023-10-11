83ffb20f39495e82e6889978b342cb8a2cda401b
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-11 16:53:12 +0000

    fix fxn def in task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,12 +4,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    val = []
-    for k, v in a_dictionary.items():
-        val.append(v)
-    m = max(val)
-
-    for a, b in a_dictionary.items():
-        if b == m:
-            return a
-    
+    return max(a_dictionary, key=a_dictionary.get)
