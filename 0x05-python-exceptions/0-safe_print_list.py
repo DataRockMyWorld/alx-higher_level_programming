8ef117f47e0e4b27a7fc239e210937bddd96f8ed
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-23 12:46:07 +0000

    make code more pythonic

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,18 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for ele in range(x):
-            if my_list[ele] == my_list[x - 1]:
-                print(my_list[ele])
-            else:
-                print(my_list[ele], end="")
-            count = count + 1
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
     except IndexError:
-        count = 0
-        for x in my_list:
-            if my_list[-1] == x:
-                print(x)
-            else:
-                print(x, end="")
-            count = count + 1
+        pass
+    print()
     return count
