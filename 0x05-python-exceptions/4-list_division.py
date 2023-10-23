bc34371aa6dcc13d7b1cc5318bc22ac512144986
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-23 16:04:48 +0000

    fix new list name

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div = 0
         finally:
-            new_list.append(div)
+            new.append(div)
     return new
