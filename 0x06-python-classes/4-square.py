0f53fdd9ac1498b0c4db991605e1e295ea165906
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-24 12:52:52 +0000

    fix task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializing the objects
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Property to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setting the attribute size"""
+        self.__size = value
