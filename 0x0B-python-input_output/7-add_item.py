bd7d1ec31b4e06d177b9b714076879a2b668559e
Jewelz_B <jewelbansah@rocketmail.com>
2023-11-07 21:09:42 +0000

    fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""""This Module adds all arguments to a python list"""
+"""This module adds all arguments to a Python list and save them to a file."""
+
 
 import sys
 
@@ -7,8 +8,9 @@ if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    my_list = []
-    for arg in sys.argv[1:]:
-        my_list.append(arg)
-
-    save_to_json_file(my_list, add_item.json)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
