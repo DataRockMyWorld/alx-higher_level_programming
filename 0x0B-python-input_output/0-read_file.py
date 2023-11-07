3d8f6cfccec6cafe3b1b23713ea751e9f47e844a
Jewelz_B <jewelbansah@rocketmail.com>
2023-11-07 16:09:17 +0000

    solve task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module reads text from a file"""
+
+
+def read_file(filename=""):
+    """Function reads text from a file & prints
+    to standard output
+    """
+    with open(filename, encoding="utf-8") as Myfile:
+        print(Myfile.read())
