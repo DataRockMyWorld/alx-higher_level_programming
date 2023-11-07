a8c2bc6bedf03175dfa2f2457c013f4c92a13ee3
Jewelz_B <jewelbansah@rocketmail.com>
2023-11-07 18:18:31 +0000

    fix task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Module returns json representation of an object string"""
 import json
 
+
 def to_json_string(my_obj):
     """Returns json representation of an object"""
     return json.dumps(my_obj)
