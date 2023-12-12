b64ed19ad37ef756972b6bf89ca1428922a38cac
Jewelz_B <jewelbansah@rocketmail.com>
2023-12-12 19:44:01 +0000

    solve task 2 - 10)

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- This script deletes database hbtn_0c_0 from my server
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Inserts New row in my table
+INSERT INTO first_table (id, name) VALUE(89, "Best School");
