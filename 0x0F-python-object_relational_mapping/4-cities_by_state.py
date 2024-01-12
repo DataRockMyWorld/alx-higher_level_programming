fc8c1523467906551160c90c6404b0be2905280e
Jewel Bansah <jewelbansah@rocketmail.com>
2024-01-12 22:58:21 +0000

    fix task 2 & 3

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,11 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4],))
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC"
+    )
 
     query_rows = cur.fetchall()
     for row in query_rows:
