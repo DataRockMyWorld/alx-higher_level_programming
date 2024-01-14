23e47ea9bf3a155f2e3840e700455da3d8b9fd79
Jewel Bansah <jewelbansah@rocketmail.com>
2024-01-14 11:33:58 +0000

    fix task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Acess to database codesource
+    Access to the database and get a state
+    from the database.
     """
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        )
-    )
-    Base.metadata.create_all(engine)
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    
-    if state is None:
-        print("Nothing")
+    if state is not None:
+        print("{0}: {1}".format(state.id, state.name))
     else:
-        print("{}:{}".format(state.id, state.name))
+        print("Nothing")
