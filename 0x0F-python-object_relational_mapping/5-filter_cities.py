64316aa555193941e671b57101c5fcfb808e7797
Jewel Bansah <jewelbansah@rocketmail.com>
2024-01-14 11:30:48 +0000

    fix task 5 & 6

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,6 +8,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    define the function
+    """
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
