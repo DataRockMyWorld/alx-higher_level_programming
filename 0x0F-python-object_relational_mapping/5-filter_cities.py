b5762b100c0ce50ee619556751ae5f60cc0bb8ed
Jewel Bansah <jewelbansah@rocketmail.com>
2024-01-14 07:59:15 +0000

    solve task 4 & 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     )
     with db.cursor() as cur:
         cur.execute(
-            "SELECT cities.id, cities.name, states.name \
-                FROM cities JOIN states ON cities.state_id \
-                    = states.id ORDER BY cities.id ASC"
+            "SELECT cities.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                    WHERE states.name LIKE BINARY %(name)s ORDER BY cities.name ASC",
+            {'name': argv[4]},
         )
 
         query_rows = cur.fetchall()
-
+    
     if query_rows is not None:
-        for row in query_rows:
-            print(row)
+       print(", ".join([row[0] for row in query_rows]))
 
     cur.close()
     db.close()
